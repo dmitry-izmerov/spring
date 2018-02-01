3a4f9e7fbf4974ad7bf2d13027840b69ebd6e9ce
demi <idd90i@gmail.com>
2018-02-01 20:45:03 +0300

    added logging by event type in app

--- spring-core/src/main/java/ru/demi/App.java
+++ spring-core/src/main/java/ru/demi/App.java
@@ -7,39 +7,52 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 import org.springframework.stereotype.Component;
 import ru.demi.logger.Event;
 import ru.demi.logger.EventLogger;
+import ru.demi.logger.EventType;
 
+import java.util.Objects;
 import java.util.concurrent.TimeUnit;
+import java.util.Map;
 
 @Component
 public class App {
     private Client client;
-    private EventLogger eventLogger;
+	private Map<EventType, EventLogger> loggers;
+	private EventLogger cacheFileEventLogger;
 
-    @Autowired
+	@Autowired
     public App(Client client, EventLogger cacheFileEventLogger) {
         this.client = client;
-        this.eventLogger = cacheFileEventLogger;
-    }
-    
+		this.cacheFileEventLogger = cacheFileEventLogger;
+	}
+
+	public void setLoggers(Map<EventType, EventLogger> loggers) {
+		this.loggers = loggers;
+	}
+
     public static void main(String[] args) throws InterruptedException {
         ConfigurableApplicationContext context = new AnnotationConfigApplicationContext(Config.class);
         App app = context.getBean(App.class);
+        app.setLoggers((Map<EventType, EventLogger>) context.getBean("loggers"));
 		Event event1 = context.getBean(Event.class);
 		Event event2 = context.getBean(Event.class);
 		Event event3 = context.getBean(Event.class);
 		event1.setMessage("Event for user 1");
-		app.logEvent(event1);
+		app.logEvent(event1, null);
 		event2.setMessage("Event for user 2");
-		app.logEvent(event2);
+		app.logEvent(event2, EventType.INFO);
 		event3.setMessage("Event for user 3");
-		app.logEvent(event3);
+		app.logEvent(event3, EventType.ERROR);
 
 		TimeUnit.SECONDS.sleep(3);
 
 		context.close();
 	}
     
-    public void logEvent(Event event) {
+    public void logEvent(Event event, EventType eventType) {
+		EventLogger eventLogger = loggers.get(eventType);
+    	if (Objects.isNull(eventLogger)) {
+			eventLogger = cacheFileEventLogger;
+		}
         String message = event.getMessage().replaceAll(client.getId(), client.getFullName());
         event.setMessage(message);
         eventLogger.logEvent(event);
