abc86de468674766a4d7577c51da18262a243773
demi <idd90i@gmail.com>
2018-03-22 14:28:07 +0300

    changed resource folders location for spring security base app

--- spring-security/src/main/java/ru/demi/base/App.java
+++ spring-security/src/main/java/ru/demi/base/App.java
@@ -1,17 +1,21 @@
 package ru.demi.base;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.annotation.Bean;
 import org.thymeleaf.extras.springsecurity4.dialect.SpringSecurityDialect;
 import org.thymeleaf.spring5.SpringTemplateEngine;
+import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
 import org.thymeleaf.templateresolver.ITemplateResolver;
 
 @SpringBootApplication
 public class App {
 
 	public static void main(String[] args) {
-		SpringApplication.run(App.class, args);
+		new SpringApplicationBuilder(App.class)
+			.properties("spring.config.location=classpath:base/app-config.yml")
+			.build()
+			.run(args);
 	}
 
 	// enable thymeleaf-extras tags in templates
@@ -21,5 +25,16 @@ public class App {
 		templateEngine.setTemplateResolver(templateResolver);
 		templateEngine.addDialect(dialect);
 		return templateEngine;
+	}
+
+	// customize thymeleaf templates location
+	@Bean
+	public ITemplateResolver templateResolver() {
+		SpringResourceTemplateResolver templateResolver = new SpringResourceTemplateResolver();
+		templateResolver.setPrefix("classpath:base/templates/");
+		templateResolver.setSuffix(".html");
+		templateResolver.setCacheable(false);
+		templateResolver.setOrder(0);
+		return templateResolver;
 	}
 }
